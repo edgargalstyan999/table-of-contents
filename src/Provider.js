0e826be22d8446df6136681a42272e6bd63f9ae4
edgargalstyan999 <edgargalstyan999@gmail.com>
2021-12-29 17:16:56 +0400

    minor fix

--- src/Provider.js
+++ src/Provider.js
@@ -1,23 +1,21 @@
 import React, { useEffect, useState, createContext } from 'react';
 import axios from 'axios';
- 
+
 export const UserContext = createContext(null);
 export const Provider = (props) => {
-       const [user, setUser] = useState(null);
-  
+  const [user, setUser] = useState(null);
+
   const fetchData = () => {
     axios
       .get(process.env.REACT_APP_URL + '/users')
       .then((response) => setUser(response.data));
   };
-  
+
   useEffect(() => {
     fetchData();
   }, []);
 
   return (
-    <UserContext.Provider value={user}>
-      {props.children}
-    </UserContext.Provider>
+    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
   );
 };
